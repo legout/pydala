3161c1af67deb8d7081611e671f98d3264ebed0a
legout <volker.lorrmann@gmail.com>
2022-10-09 12:27:15 +0200

    Dataset Reader and Writer added

--- src/pydatalake/dataset.py
+++ src/pydatalake/dataset.py
@@ -76,29 +76,30 @@ class Reader:
                     self._pa_table = self._pa_dataset.to_table(**kwargs)
 
         self.ddb.register(name, self._pa_table)
-        
-    def create_temp_table(self, name:str="temp_table", **kwargs):
+
+    def create_temp_table(self, name: str = "temp_table", **kwargs):
         if hasattr(self, "_pa_table"):
-            self.execute(f"CREATE OR REPLACE TEMP TABLE {name} AS SELECT * FROM pa_table")
-            
-        else:
-            if not hasattr(self, '_pa_dataset'):
-                self._load_dataset(**kwargs)
-                
             self.execute(
-                    f"CREATE OR REPLACE TEMP TABLE {name} AS SELECT * FROM pa_dataset"
-                )
-    
+                f"CREATE OR REPLACE TEMP TABLE {name} AS SELECT * FROM pa_table"
+            )
+
+        else:
+            if not hasattr(self, "_pa_dataset"):
+                self._load_dataset(**kwargs)
+
+            self.execute(
+                f"CREATE OR REPLACE TEMP TABLE {name} AS SELECT * FROM pa_dataset"
+            )
+
     def query(self, *args, **kwargs):
         return self.ddb.query(*args, **kwargs)
-    
+
     def execute(self, *args, **kwargs):
         return self.ddb.execute(*args, **kwargs)
-    
+
     def filter(self, *args, **kwargs):
         return self.ddb_relation.filter(*args, **kwargs)
-        
-    
+
     @property
     def pa_dataset(self, **kwargs):
         if not hasattr(self, "_pa_dataset") or len(kwargs) > 0:
@@ -108,7 +109,7 @@ class Reader:
 
     @property
     def pa_table(self, **kwargs):
-        
+
         if not hasattr(self, "_pa_table") or len(kwargs) > 0:
             name = kwargs.get("name", "temp_table")
             if name in self.execute("SHOW TABLES").df()["name"].tolist():
@@ -117,75 +118,130 @@ class Reader:
                 self._load_table(**kwargs)
 
         return self._pa_table
-    
+
     @property
     def ddb_relation(self, **kwargs):
         name = kwargs.get("name", "temp_table")
         if name in self.execute("SHOW TABLES").df()["name"].tolist():
             return self.query(f"SELECT * FROM {name}")
-        
+
         elif hasattr(self, "_pa_table"):
             return self.ddb.from_arrow(self._pa_table)
         else:
-            if not hasattr(self, "_pa_dataset") or len(kwargs)>0:
+            if not hasattr(self, "_pa_dataset") or len(kwargs) > 0:
                 self._load_dataset(**kwargs)
-                
+
             return self.ddb.from_arrow(self._pa_dataset)
-        
+
     @property
     def pl_dataframe(self, **kwargs):
         return pl.from_arrow(self.pa_table(**kwargs))
-            
-        
 
-       
-    
-    
-    
+
 class Writer:
-    
+    def __init__(
+        self,
+        path: str,
+        base_name: str = "data",
+        partitioning: ds.Partitioning | list[str] | str | None = None,
+        with_time_partition: bool = False,
+        filesystem: fs.FileSystem | None = None,
+        format: str | None = "parquet",
+        compression: str | None = "zstd",
+    ):
+        self._path = path
+        self._base_name = base_name
+        self._partitioning = partitioning
+        self._with_time_partition = with_time_partition
+        self._filesystem = filesystem
+        self._format = format
+        self._compression = compression
 
     def _gen_path(
         self,
-        path: str | None = None,
-        base_name: str = "data",
         partition_names: tuple | None = None,
-        partition_by_date: bool = False,
     ):
-        if path is None:
-            path = self._path
-        parts = [path]
+
+        parts = [self._path]
 
         if partition_names is not None:
             parts.extend(partition_names)
 
-        if partition_by_date:
-            parts.append(str(dt.date.today()))
+        if self._with_time_partition:
+            parts.append(str(dt.datetime.today()))
 
-        parts.append(base_name + f"-{uuid.uuid4().hex}")
+        parts.append(self._base_name)# + f"-{uuid.uuid4().hex}.{self._format}")
 
         path_ = Path(*parts)
 
         if self._filesystem is None:
-            path_.parent.mkdir(exist_ok=True, parents=True)
+            path_.mkdir.parents(exist_ok=True, parents=True)
 
         return path
+
+    def _to_duckdbrelation(
+        self,
+        table: duckdb.DuckDBPyRelation
+        | pa.Table
+        | ds.Dataset
+        | pd.DataFrame
+        | pl.DataFrame
+        | str,
+        # set_temp_table: bool=False,
+    ):
+        if isinstance(table, pa.Table):
+            table_ = self.ddb.from_arrow(table)
+        elif isinstance(table, ds.Dataset):
+            _table = table
+            table_ = self.query("SELECT * FROM _table")
+        elif isinstance(table, pd.DataFrame):
+            table_ = self.ddb.from_df(table)
+        elif isinstance(table, pl.DataFrame):
+            table_ = self.ddb.from_arrow(table.to_arrow())
+        elif isinstance(table, str):
+            if ".parquet" in table:
+                table_ = self.ddb.from_parquet(table)
+            elif ".csv" in table:
+                table_ = self.ddb.from_csv_auto(table)
+            else:
+                table_ = self.query(f"SELECT * FROM '{table}'")
+        else:
+            table_ = table
+
+        return table_
+
+    def _make_temp_table(
+        self,
+        table: duckdb.DuckDBPyRelation
+        | pa.Table
+        | ds.Dataset
+        | pd.DataFrame
+        | pl.DataFrame
+        | str,
+        name: str = "temp_table",
+    ):
+        table_ = self._to_duckdbrelation(table, use_temp_table=False)
+        self.execute(f"CREATE OR REPLACE TEMP TABLE {name} AS SELECT * FROM table_")
 
     def write_table(
         self,
         table: pa.Table,
         path: Path | str,
-        compression: str = "zstd",
         **kwargs,
     ):
 
         filesystem = kwargs.get("filesystem", self._filesystem)
+        compression = kwargs.get("compression", self._compression)
         format = (
             kwargs.get("format", self._format)
             .replace("arrow", "feather")
             .replace("ipc", "feather")
         )
-        path = str(path) + f".{format}"
+        if isinstance(Path, str):
+            path = Path(path)
+
+        if path.suffix == "":
+            path = path / self._base_name + f"-{uuid.uuid4().hex}.{self._format}"
 
         if format == "feather":
             if filesystem is not None:
@@ -212,54 +268,6 @@ class Writer:
                 **kwargs,
             )
 
-    def _to_duckdbrelation(
-        self,
-        table: duckdb.DuckDBPyRelation
-        | pa.Table
-        | ds.Dataset
-        | pd.DataFrame
-        | pl.DataFrame
-        | str,
-        use_temp_table: bool,
-    ):
-        if isinstance(table, pa.Table):
-            table_ = self.ddb.from_arrow(table)
-        elif isinstance(table, ds.Dataset):
-            _table = table
-            table_ = self.query("SELECT * FROM _table")
-        elif isinstance(table, pd.DataFrame):
-            table_ = self.ddb.from_df(table)
-        elif isinstance(table, pl.DataFrame):
-            table_ = self.ddb.from_arrow(table.to_arrow())
-        elif isinstance(table, str):
-            if ".parquet" in table:
-                table_ = self.ddb.from_parquet(table)
-            else:
-                table_ = self.query(f"SELECT * FROM '{table}'")
-        else:
-            table_ = table
-
-        if use_temp_table:
-            self.execute(
-                "CREATE OR REPLACE TEMP TABLE temp_table AS SELECT * FOM table_"
-            )
-            table_ = self.query("SELECT * FROM temp_table")
-
-        return table_
-    
-    def temp_table(self, table_: duckdb.DuckDBPyRelation
-        | pa.Table
-        | ds.Dataset
-        | pd.DataFrame
-        | pl.DataFrame
-        | str, name:str="temp_table"):
-        
-        self.execute(
-                "CREATE OR REPLACE TEMP TABLE temp_table AS SELECT * FOM table_"
-            )
-            table_ = self.query(f"SELECT * FROM {name}")
-        
-
     def write_dataset(
         self,
         table: duckdb.DuckDBPyRelation
@@ -268,83 +276,67 @@ class Writer:
         | pd.DataFrame
         | pl.DataFrame
         | str,
-        path: str | None,
-        repartitioning: list | str | None = None,
-        partition_by_date: bool = False,
+        path: str | None = None,
+        partitioning: list | str | None = None,
+        with_time_partition: bool = False,
         n_rows: int | None = None,
-        use_temp_table: bool = True,
+        with_temp_table: bool = True,
         **kwargs,
     ):
-        table_ = self._to_duckdbrelation(table=table, use_temp_table=use_temp_table)
-
-        if repartitioning is not None:
+        
+            
+        format = kwargs.get("format", self._format)
+        compression = kwargs.get("compression", self._compression)
+        
+        table_ = self._to_duckdbrelation(table=table)
+        if with_temp_table:
+            self._make_temp_table(table_)
+            
+        if partitioning is not None:
             if isinstance(repartitioning, str):
-                self._partitioning = [repartitioning]
-            else:
-                self._partitioning = repartitioning
+                partitioning = [partitioning]
+        else:
+            partitioning = self._partitioning
 
-        if self._partitioning is not None:
-            partitions = (
-                table_.project(",".join(self._partitioning)).distinct().fetchall()
-            )
+        if partitioning is not None:
+            partitions = table_.project(",".join(partitioning)).distinct().fetchall()
 
             for partition_names in partitions:
                 path_ = self._gen_path(
                     path=path,
                     partition_names=partition_names,
-                    partition_by_date=partition_by_date,
                 )
 
                 filter_ = []
-                for p in zip(self._partitioning, partition_names):
+                for p in zip(partitioning, partition_names):
                     filter_.append(f"{p[0]}='{p[1]}'")
                 filter_ = " AND ".join(filter_)
 
                 table_part = table_.filter(filter_)
 
                 if n_rows is None:
-                    self.write_table(table=table_part.arrow(), path=path_, **kwargs)
+                    self.write_table(
+                        table=table_part.arrow(),
+                        path=path_,
+                        format=format,
+                        compression=compression,
+                        **kwargs,
+                    )
                 else:
                     for i in range(table_part.shape[0] // n_rows + 1):
                         self.write_table(
                             table=table_part.limit(n_rows, offset=i * n_rows).arrow(),
-                            path=str(path_) + f"-{i}",
+                            path=path_,
+                            format=format,
+                            compression=compression,
                             **kwargs,
                         )
 
         else:
             path_ = self._gen_path(
-                path=path, partition_names=None, partition_by_date=partition_by_date
+                path=path, partition_names=None
             )
             self.write_table(table=table_.arrow(), path=path_, **kwargs)
 
-        if use_temp_table:
+        if with_temp_table:
             self.execute("DROP TABLE temp_table")
-
-    def append(
-        self,
-        table: duckdb.DuckDBPyRelation
-        | pa.Table
-        | ds.Dataset
-        | pd.DataFrame
-        | pl.DataFrame
-        | str,
-        repartitioning: list | str | None = None,
-        partition_by_date: bool = False,
-        n_rows: int | None = None,
-        use_temp_table: bool = True,
-        **kwargs,
-    ):
-        
-
-        self.write_dataset(
-            table=table,
-            path=None,
-            repartitioning=repartitioning,
-            partition_by_date=partition_by_date,
-            n_rows=n_rows,
-            use_temp_table=use_temp_table,
-            **kwargs,
-        )
-
-
