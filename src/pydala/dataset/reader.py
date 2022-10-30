133e24594aca36bc8e6b94aec0b2a1b56c1ace31
legout <volker.lorrmann@gmail.com>
2022-10-30 20:46:06 +0100

    Started TimeFly feature

--- src/pydala/dataset/reader.py
+++ src/pydala/dataset/reader.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 from tempfile import mkdtemp
 
@@ -8,7 +9,9 @@ import pyarrow as pa
 import pyarrow.dataset as ds
 import pyarrow.feather as pf
 import pyarrow.parquet as pq
+import pytz
 import s3fs
+import toml
 
 from ..filesystem.filesystem import FileSystem
 from .utils import (distinct_table, drop_columns, get_ddb_sort_str, sort_table,
@@ -261,7 +264,9 @@ class Reader:
 
             if self._sort_by is not None:
 
-                sort_by = get_ddb_sort_str(sort_by=self._sort_by, ascending=self._ascending)
+                sort_by = get_ddb_sort_str(
+                    sort_by=self._sort_by, ascending=self._ascending
+                )
 
                 sql += f" ORDER BY {sort_by}"
 
@@ -530,3 +535,80 @@ class Reader:
     @property
     def cached(self) -> bool:
         return self._cached
+
+
+class TimeFlyReader(Reader):
+    def __init__(
+        self,
+        path: str,
+        timefly: str | dt.datetime | None = None,
+        bucket: str | None = None,
+        name: str | None = None,
+        partitioning: ds.Partitioning | str | None = None,
+        filesystem: FileSystem | None = None,
+        format: str | None = "parquet",
+        sort_by: str | list | None = None,
+        ascending: bool | list | None = None,
+        distinct: bool | None = None,
+        drop: str | list | None = "__index_level_0__",
+        ddb: duckdb.DuckDBPyConnection | None = None,
+        caching: bool = False,
+        cache_prefix: str | None = "/tmp/pydala/",
+    ):
+        bucket = bucket or ""
+
+        if filesystem.exists(os.path.join(bucket, path, "timefly.toml")):
+
+            with open(os.path.join(path, "timefly.toml")) as f:
+                self._timefly = toml.load(f)
+
+            subpath = self._find_timefly_subpath(timefly=timefly)
+            path = os.path.join(path, subpath)
+        else:
+            self._timefly = False
+
+        super().__init__(
+            path=path,
+            bucket=bucket,
+            name=name,
+            partitioning=partitioning,
+            filesystem=filesystem,
+            format=format,
+            sort_by=sort_by,
+            ascending=ascending,
+            distinct=distinct,
+            drop=drop,
+            ddb=ddb,
+            caching=caching,
+            cache_prefix=cache_prefix,
+        )
+
+    def _find_timefly_subpath(self, timefly: str | dt.datetime | None):
+
+        timefly = timefly or "current"
+
+        if timefly != "current":
+            if isinstance(timefly, str):
+                timefly_timestamp = dt.datetime.strptime(timefly, "%Y%m%d_%H%M%S")
+            else:
+                timefly_timestamp = timefly
+
+            all_timestamps = [
+                dt.datetime.strptime(subpath, "%Y%m%d_%H%M%S")
+                for subpath in self._timefly["dataset"]["subpaths"]["all"]
+            ]
+            timefly_diff = sorted(
+                [
+                    timefly_timestamp - timestamp
+                    for timestamp in all_timestamps
+                    if timestamp < timefly_timestamp
+                ]
+            )[0]
+            timefly_subpath = (timefly_timestamp - timefly_diff).strftime(
+                "%Y%m%d_%H%M%S"
+            )
+
+        else:
+            timefly_subpath = "current"
+
+        return os.path.join(self._path, timefly_subpath)
---
+++ src/pydala/dataset/timefly.py
@@ -0,0 +1,103 @@
+import datetime as dt
+import os
+
+import duckdb
+import pyarrow.dataset as ds
+import pytz
+import toml
+from writer import Writer
+
+from ..filesystem.filesystem import FileSystem
+from .reader import Reader
+
+
+class ConfigReader
+
+
+class DatasetReader(Reader):
+    def __init__(
+        self,
+        path: str,
+        timefly: str | dt.datetime | None = None,
+        bucket: str | None = None,
+        name: str | None = None,
+        partitioning: ds.Partitioning | str | None = None,
+        filesystem: FileSystem | None = None,
+        format: str | None = "parquet",
+        sort_by: str | list | None = None,
+        ascending: bool | list | None = None,
+        distinct: bool | None = None,
+        drop: str | list | None = "__index_level_0__",
+        ddb: duckdb.DuckDBPyConnection | None = None,
+        caching: bool = False,
+        cache_prefix: str | None = "/tmp/pydala/",
+    ):
+        
+        self._timefly = self._read_timefly(filesystem=filesystem, path=path, bucket=bucket)
+
+        if self._timefly is not None:
+            subpath = self._find_timefly_subpath(timefly=timefly)
+            path = os.path.join(path, subpath)
+        else:
+            self._timefly = False
+
+        super().__init__(
+            path=path,
+            bucket=bucket,
+            name=name,
+            partitioning=partitioning,
+            filesystem=filesystem,
+            format=format,
+            sort_by=sort_by,
+            ascending=ascending,
+            distinct=distinct,
+            drop=drop,
+            ddb=ddb,
+            caching=caching,
+            cache_prefix=cache_prefix,
+        )
+
+    def _read_timefly(self, filesystem:FileSystem|None, path:str, bucket:str|None=None):
+        if filesystem is None:
+            filesystem = FileSystem(type_="local")
+        
+        bucket = bucket or ""
+
+        if filesystem.exists(os.path.join(bucket, path, "timefly.toml")):
+
+            with open(os.path.join(path, "timefly.toml")) as f:
+                return toml.load(f)
+
+    def _find_timefly_subpath(self, timefly: str | dt.datetime | None):
+
+        timefly = timefly or "current"
+
+        if timefly != "current":
+            if isinstance(timefly, str):
+                timefly_timestamp = dt.datetime.strptime(timefly, "%Y%m%d_%H%M%S")
+            else:
+                timefly_timestamp = timefly
+
+            all_timestamps = [
+                dt.datetime.strptime(subpath, "%Y%m%d_%H%M%S")
+                for subpath in self._timefly["dataset"]["subpaths"]["all"]
+            ]
+            timefly_diff = sorted(
+                [
+                    timefly_timestamp - timestamp
+                    for timestamp in all_timestamps
+                    if timestamp < timefly_timestamp
+                ]
+            )[0]
+            timefly_subpath = (timefly_timestamp - timefly_diff).strftime(
+                "%Y%m%d_%H%M%S"
+            )
+
+        else:
+            timefly_subpath = "current"
+
+        return os.path.join(self._path, timefly_subpath)
+
+
+class DatasetWriter(Writer):
+    pass
